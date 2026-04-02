30b2e53e3524be0e612c03129cd2e63ea30c1209
ShafinRME <shafin.rmedu@gmail.com>
2026-04-02 21:13:07 +0600

    user prescription route is done

--- src/app/modules/prescription/prescription.service.ts
+++ src/app/modules/prescription/prescription.service.ts
@@ -2,6 +2,7 @@ import { AppointmentStatus, PaymentStatus, Prescription, UserRole } from "@prism
 import { IJWTPayload } from "../../types/common";
 import ApiError from "../../errors/ApiError";
 import httpStatus from 'http-status'
+import { IOptions, paginationHelper } from "../../helper/paginationHelper";
 import prisma from "../../shared/prisma";
 
 const createPrescription = async (user: IJWTPayload, payload: Partial<Prescription>) => {
@@ -37,8 +38,48 @@ const createPrescription = async (user: IJWTPayload, payload: Partial<Prescripti
     return result;
 }
 
-// get my prescription as a patient
+const patientPrescription = async (user: IJWTPayload, options: IOptions) => {
+    const { limit, page, skip, sortBy, sortOrder } = paginationHelper.calculatePagination(options);
+
+    const result = await prisma.prescription.findMany({
+        where: {
+            patient: {
+                email: user.email
+            }
+        },
+        skip,
+        take: limit,
+        orderBy: {
+            [sortBy]: sortOrder
+        },
+        include: {
+            doctor: true,
+            patient: true,
+            appointment: true
+        }
+    })
+
+    const total = await prisma.prescription.count({
+        where: {
+            patient: {
+                email: user.email
+            }
+        }
+    })
+
+    return {
+        meta: {
+            total,
+            page,
+            limit
+        },
+        data: result
+    }
+
+};
+
 
 export const PrescriptionService = {
-    createPrescription
+    createPrescription,
+    patientPrescription
 }
