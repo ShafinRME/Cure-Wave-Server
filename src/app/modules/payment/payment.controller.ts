553de03f6ad1380504406ca7bce7d1079ff36b35
ShafinRME <shafin.rmedu@gmail.com>
2026-04-02 08:37:29 +0600

    stripe payment gateway is fully setup

---
+++ src/app/modules/payment/payment.controller.ts
@@ -0,0 +1,31 @@
+import { Request, Response } from "express";
+import catchAsync from "../../shared/catchAsync";
+import { PaymentService } from "./payment.service";
+import sendResponse from "../../shared/sendResponse";
+import { stripe } from "../../helper/stripe";
+
+const handleStripeWebhookEvent = catchAsync(async (req: Request, res: Response) => {
+
+    const sig = req.headers["stripe-signature"] as string;
+    const webhookSecret = "whsec_6de2f649b51a0108c58db494ca6d2e2333abc6e83f202577b318e72736d7cbdc";
+
+    let event;
+    try {
+        event = stripe.webhooks.constructEvent(req.body, sig, webhookSecret);
+    } catch (err: any) {
+        console.error("Webhook signature verification failed:", err.message);
+        return res.status(400).send(`Webhook Error: ${err.message}`);
+    }
+    const result = await PaymentService.handleStripeWebhookEvent(event);
+
+    sendResponse(res, {
+        statusCode: 200,
+        success: true,
+        message: 'Webhook req send successfully',
+        data: result,
+    });
+});
+
+export const PaymentController = {
+    handleStripeWebhookEvent
+}
