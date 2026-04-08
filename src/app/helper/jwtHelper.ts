54ebe84ae16e59c728592b9d226a55b425b4e2f6
ShafinRME <shafin.rmedu@gmail.com>
2026-04-08 23:24:43 +0600

    rate limiting edited

--- src/app/helper/jwtHelper.ts
+++ src/app/helper/jwtHelper.ts
@@ -1,6 +1,6 @@
 import jwt, { JwtPayload, Secret, SignOptions } from "jsonwebtoken";
 
-const generateToken = (payload: any, secret: Secret, expiresIn: string) => {
+const geneToken = (payload: any, secret: Secret, expiresIn: string) => {
     const token = jwt.sign(payload, secret, {
         algorithm: "HS256",
         expiresIn
